15f5895527f4337365c3d7ae4e00b2d032eb749a
0x-cygnet <bloopsocket@gmail.com>
2021-10-22 13:44:27 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React #1
         </a>
       </header>
     </div>
